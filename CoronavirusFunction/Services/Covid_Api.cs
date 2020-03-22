c4799b6f93149ffbebf5584a067ff46908890c66
lorenzogiudici5 <lorenzogiudici5@gmail.com>
2020-03-22 17:39:57 +0000

    Get data from github repo AB#101

---
+++ CoronavirusFunction/Services/Covid_Api.cs
@@ -0,0 +1,41 @@
+﻿using CoronavirusFunction.Models;
+using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoronavirusFunction.Services
+{
+    public static class Covid_Api
+    {
+        private const string BASEURL = "https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-json";
+
+        public static async Task<DatiNazionali> GetCountryData(string country, DateTime? date = null)
+        {
+            var pcmDcpApi = RestService.For<IPcmDpcApi>(BASEURL);
+            var dati = !date.HasValue ? await pcmDcpApi.GetLastDatiNazionali() : await pcmDcpApi.GetDatiNazionali();
+            return !date.HasValue ?
+                dati.FirstOrDefault(x => x.Name == country) : 
+                dati.FirstOrDefault(x => x.Name == country && x.Data.Date == date.Value);
+        }
+
+        public static async Task<DatiRegionali> GetAdminAreaData(string area, DateTime? date = null)
+        {
+            var pcmDcpApi = RestService.For<IPcmDpcApi>(BASEURL);
+            var dati = !date.HasValue ? await pcmDcpApi.GetLastDatiRegionali() : await pcmDcpApi.GetDatiRegionali();
+            return !date.HasValue ?
+                dati.FirstOrDefault(x => x.Name == area) :
+                dati.FirstOrDefault(x => x.Name == area && x.Data.Date == date.Value);
+        }
+
+        public static async Task<DatiProvinciali> GetDistrictArea(string district, DateTime? date = null)
+        {
+            var pcmDcpApi = RestService.For<IPcmDpcApi>(BASEURL);
+            var dati = !date.HasValue ? await pcmDcpApi.GetLastDatiProvinciali() : await pcmDcpApi.GetDatiProvinciali();
+            return !date.HasValue ?
+                dati.FirstOrDefault(x => x.Name == district) :
+                dati.FirstOrDefault(x => x.Name == district && x.Data.Date == date.Value);
+        }
+    }
+}
---
+++ CoronavirusFunction/Services/IPcmDpcApi.cs
@@ -0,0 +1,32 @@
+﻿using CoronavirusFunction.Models;
+using Newtonsoft.Json;
+using Refit;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CoronavirusFunction.Services
+{
+    public interface IPcmDpcApi
+    {
+        private const string URL_NAZIONE_LATEST = "/dpc-covid19-ita-andamento-nazionale-latest.json";
+        private const string URL_NAZIONE = "/dpc-covid19-ita-andamento-nazionale.json";
+        private const string URL_REGIONE_LATEST = "/dpc-covid19-ita-regioni-latest.json";
+        private const string URL_REGIONE = "/dpc-covid19-ita-regioni.json";
+        private const string URL_PROVINCIA_LATEST = "/dpc-covid19-ita-province-latest.json";
+        private const string URL_PROVINCIA = "/dpc-covid19-ita-regioni.json";
+
+        [Get("/dpc-covid19-ita-andamento-nazionale.json")]
+        Task<IEnumerable<DatiNazionali>> GetDatiNazionali();
+        [Get("/dpc-covid19-ita-regioni.json")]
+        Task<IEnumerable<DatiRegionali>> GetDatiRegionali();
+        [Get("/dpc-covid19-ita-province.json")]
+        Task<IEnumerable<DatiProvinciali>> GetDatiProvinciali();
+
+        [Get("/dpc-covid19-ita-andamento-nazionale-latest.json")]
+        Task<IEnumerable<DatiNazionali>> GetLastDatiNazionali();
+        [Get("/dpc-covid19-ita-regioni-latest.json")]
+        Task<IEnumerable<DatiRegionali>> GetLastDatiRegionali();
+        [Get("/dpc-covid19-ita-province-latest.json")]
+        Task<IEnumerable<DatiProvinciali>> GetLastDatiProvinciali();
+    }
+}
