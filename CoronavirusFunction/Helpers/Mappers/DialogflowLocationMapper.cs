131cdc6f84fa880293edc89b443347fb029442a1
lorenzogiudici5 <lorenzogiudici5@gmail.com>
2020-03-25 15:56:28 +0000

    Fix dialogflow location mapping

--- CoronavirusFunction/Helpers/Mappers/DialogflowLocationMapper.cs
+++ CoronavirusFunction/Helpers/Mappers/DialogflowLocationMapper.cs
@@ -6,7 +6,12 @@ namespace CoronavirusFunction.Helpers
     {
         public static Location ToLocation(this DialogflowLocationDto locationDto)
         {
-            var subAdminArea = !string.IsNullOrEmpty(locationDto.SubadminArea) ? locationDto.SubadminArea : (!string.IsNullOrEmpty(locationDto.BusinessName) && !string.IsNullOrEmpty(locationDto.StreetAddress)) ? $"{locationDto.BusinessName} di {locationDto.StreetAddress}" : default(string);
+            var subAdminArea = !string.IsNullOrEmpty(locationDto.SubadminArea) ? locationDto.SubadminArea : 
+                (!string.IsNullOrEmpty(locationDto.BusinessName) && !string.IsNullOrEmpty(locationDto.StreetAddress)) ? 
+                $"{locationDto.BusinessName} di {locationDto.StreetAddress}" :
+                (!string.IsNullOrEmpty(locationDto.BusinessName) && !string.IsNullOrEmpty(locationDto.City)) ?
+                $"{locationDto.BusinessName} di {locationDto.City}" :
+                default(string);
 
             return new Location()
             {
