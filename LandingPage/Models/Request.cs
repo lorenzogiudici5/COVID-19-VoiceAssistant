8a44b92198ddd57657ca611f6fe9d2a3be451b3a
lorenzogiudici5 <lorenzogiudici5@gmail.com>
2020-04-15 18:14:42 +0000

    Improvements with style and component

---
+++ LandingPage/Models/Request.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LandingPage.Models
+{
+    public class Request
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Invalid Email Address")]
+        public string Email { get; set; }
+        public bool Alexa { get; set; }
+        public bool Google { get; set; }
+    }
+}
